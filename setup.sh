23d035b65e8edf8622442f43cef375ea40f3f5b5
Jacob <ge3224@gmail.com>
2023-03-09 20:51:10 -0600

    Update setup script

--- setup.sh
+++ setup.sh
@@ -8,10 +8,15 @@ echo "Starting setup..."
 # Enter Bios - Config > Keyboard/Mouse > Fn and Ctrl Key Swap
 
 # ---------------------Make CapsLock Esc------------------------
-sudo sed -i '/Option "XkbOptions" "terminate:ctrl_alt_bksp/Option "XkbOptions" "terminate:ctrl_alt_bksp,caps:escape_shifted_capslock/g"' sudo nvim /etc/X11/xorg.conf.d/00-keyboard.conf
+# sudo sed -i 's/terminate:ctrl_alt_bksp/terminate:ctrl_alt_bksp,caps:escape_shifted_capslock/g' > sudo /etc/X11/xorg.conf.d/00-keyboard.conf
+#
+# Undo
+# sudo sed -i 's/terminate:ctrl_alt_bksp,caps:escape_shifted_capslock/terminate:ctrl_alt_bksp/g' > sudo /etc/X11/xorg.conf.d/00-keyboard.conf
 
 # -------------Accessing the clipboard in Neovim----------------
 # sudo pacman -S xclip
+#
+# Undo:
 # sudo pacman -Rs xclip
 
 # -----------------------USB mounting---------------------------
@@ -21,4 +26,41 @@ sudo sed -i '/Option "XkbOptions" "terminate:ctrl_alt_bksp/Option "XkbOptions" "
 # - `sudo mkfs.vfat /dev/<drive_name>` - format a drive
 # - `udiskie` will startup via i3 config
 # sudo pacman -S udisks2 udiskie
+#
+# Undo:
 # sudo pacman -Rs udisks2 udiskie
+
+# -------------------------LightDM------------------------------
+# sudo pacman -S lightdm
+#
+# Undo:
+# sudo pacman -Rs lightdm
+
+# -----------------LightDM Webkit2 Greeter----------------------
+# sudo pacman -S lightdm-webkit2-greeter
+#
+# Undo:
+# sudo pacman -Rs lgithdm-webkit2-greeter
+
+# --------------------------Keepass-----------------------------
+sudo pacman -S keepass
+#
+# Undo:
+# sudo pacman -Rs keepass
+
+# -----------------------Home Folders---------------------------
+# mkdir $HOME/Projects
+# mkdir $HOME/Documents
+# mkdir $HOME/Downloads
+# mkdir $HOME/Design
+# mkdir $HOME/Desktop
+
+# ------------------Nerd Fonts Jet Brains-----------------------
+# cd $HOME/Downloads/
+# git clone https://aur.archlinux.org/nerd-fonts-jetbrains-mono-160.git
+# cd ./nerd-fonts-jetbrains-mono-160/
+# makepkg
+# sudo pacman -U nerd-fonts-jetbrains-mono-*.tar.zst
+#
+# Undo:
+# sudo pacman -Rs nerd-fonts-jetbrains-mono-160
